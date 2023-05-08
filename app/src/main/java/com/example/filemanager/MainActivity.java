60a17ab3d5eb1182d52c68e45e9dda7d2bf2f094
nasty <nastyakras1707@gmail.com>
2023-05-08 21:12:03 +0300

    FirstCommit

--- app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -32,12 +32,12 @@ public class MainActivity extends AppCompatActivity {
     static int STORAGE_PERMISSION_CODE = 100;
     static String MAIN_DIRECTORY=Environment.getExternalStorageDirectory().getAbsolutePath();
 
-    TextView filesNotFound, pathTextView;
+    TextView filesNotFound, pathTextView, sortText, filterText;
     RecyclerView recyclerView;
     ArrayList<UserFile> filesAll;
     ArrayList<UserFile> currentFiles;
     AdapterRecyclerView adapter;
-    ImageView sortButton, filterButton;
+    ImageView sortButton, filterButton, home;
 
     boolean isPermissionGiven=false;
     boolean wereFilesUpdated=false;
@@ -77,9 +77,19 @@ public class MainActivity extends AppCompatActivity {
 
         sortButton=findViewById(R.id.sort_button);
         sortButton.setOnClickListener(v -> showPopupMenuForSorting());
+        sortText=findViewById(R.id.sort_text);
 
         filterButton=findViewById(R.id.filter_button);
         filterButton.setOnClickListener(v -> showPopupMenuForFilter());
+        filterText=findViewById(R.id.filter_text);
+
+        home=findViewById(R.id.home);
+        home.setOnClickListener(v -> {
+            currentFiles=filesAll=fileHelper.convertToUserFiles(filesProvider.getFilesFromChosenDirectory(MAIN_DIRECTORY));
+            path=MAIN_DIRECTORY;
+            pathTextView.setText("");
+            showRecyclerViews(currentFiles);
+        });
 
         Timer t = new Timer();
         Handler handler = new Handler(Looper.getMainLooper());
@@ -127,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
     private void showFilesByExtension(String extension){
         currentFiles = new ArrayList<>(new FilesFilter(filesAll).filterByExtension(extension));
         showRecyclerViews(currentFiles);
+        sortText.setText(R.string.defaultSort);
     }
 
     private void showRecyclerViews(ArrayList<UserFile> files) {
@@ -197,21 +208,26 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.itemSizeAsc:
                         sortedFiles = new ArrayList<>(new FilesSorter(currentFiles).sortBySizeAsc());
+                        sortText.setText(R.string.sizeAsc);
                         setRecyclerViewAdapter(sortedFiles);
                         break;
                     case R.id.itemSizeDesc:
                         sortedFiles = new ArrayList<>(new FilesSorter(currentFiles).sortBySizeDesc());
+                        sortText.setText(R.string.sizeDesc);
                         setRecyclerViewAdapter(sortedFiles);
                         break;
                     case R.id.itemDateDesc:
                         sortedFiles = new ArrayList<>(new FilesSorter(currentFiles).sortByDateDesc());
+                        sortText.setText(R.string.DateDesc);
                         setRecyclerViewAdapter(sortedFiles);
                         break;
                     case R.id.itemDateAsc:
                         sortedFiles = new ArrayList<>(new FilesSorter(currentFiles).sortByDateAsc());
+                        sortText.setText(R.string.DateAsc);
                         setRecyclerViewAdapter(sortedFiles);
                         break;
                     default:
+                        sortText.setText(R.string.defaultSort);
                         showRecyclerViews(currentFiles);
                 }
                 return true;
@@ -230,30 +246,39 @@ public class MainActivity extends AppCompatActivity {
                     switch (item.getItemId()) {
                         case R.id.jpg:
                             showFilesByExtension(".jpg");
+                            filterText.setText(R.string.jpgExtension);
                             break;
                         case R.id.docx:
                             showFilesByExtension(".doc");
+                            filterText.setText(R.string.docExtension);
                             break;
                         case R.id.svg:
                             showFilesByExtension(".svg");
+                            filterText.setText(R.string.svgExtension);
                             break;
                         case R.id.mp3:
                             showFilesByExtension(".mp3");
+                            filterText.setText(R.string.mp3Extension);
                             break;
                         case R.id.pdf:
                             showFilesByExtension(".pdf");
+                            filterText.setText(R.string.pdfExtension);
                             break;
                         case R.id.png:
                             showFilesByExtension(".png");
+                            filterText.setText(R.string.pngExtension);
                             break;
                         case R.id.txt:
                             showFilesByExtension(".txt");
+                            filterText.setText(R.string.txtExtension);
                             break;
                         case R.id.zip:
                             showFilesByExtension(".zip");
+                            filterText.setText(R.string.zipExtension);
                             break;
                         case R.id.any:
                             currentFiles=filesAll;
+                            filterText.setText(R.string.anyExtension);
                             showRecyclerViews(currentFiles);
                             break;
                     }
